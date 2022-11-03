a948ef85cafa142a06764ecec531bf3f7568f454
MinoUni <maks.reva.ua@gmail.com>
2022-11-03 19:38:20 +0200

    Refactor logic of signup, login methods, remove redundant methods, add findAllUsers method, refactor logger

--- src/main/java/com/cargodelivery/service/impl/UserServiceImpl.java
+++ src/main/java/com/cargodelivery/service/impl/UserServiceImpl.java
@@ -2,79 +2,67 @@ package com.cargodelivery.service.impl;
 
 import com.cargodelivery.dao.UserDao;
 import com.cargodelivery.dao.entity.User;
-import com.cargodelivery.dao.impl.UserDaoImpl;
 import com.cargodelivery.exception.DBException;
 import com.cargodelivery.exception.UserServiceException;
 import com.cargodelivery.security.PasswordEncoder;
 import com.cargodelivery.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.util.Optional;
+import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDao userRepository = new UserDaoImpl();
+    private static final Logger LOG = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    private static final Logger logger = LogManager.getLogger();
+    private final UserDao userRepository;
 
-    @Override
-    public Optional<User> findUser(User user) {
-        return Optional.empty();
-    }
-
-    @Override
-    public String checkRequestParam(HttpServletRequest request, String param) throws IllegalArgumentException {
-        String result = request.getParameter(param);
-        if (result == null || result.isBlank()) {
-            logger.log(Level.ERROR, String.format("Request param Blank or null par=%s res=%s", param, result));
-            throw new IllegalArgumentException(String.format("Request param Blank or null par=%s res=%s", param, result));
-        }
-        return result;
+    public UserServiceImpl(UserDao userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public void signup(User user) throws UserServiceException {
         try {
-            if (!userRepository.isExist(user)) {
-                user.setPassword(PasswordEncoder.hash(user.getPassword()));
-                userRepository.create(user);
-            } else {
-                logger.log(Level.ERROR, "User already exist");
-                throw new UserServiceException("User already exist");
+            if (userRepository.isExist(user)) {
+                throw new UserServiceException(String.format("User with id=%s already exist", user.getId()));
             }
+            user.setPassword(PasswordEncoder.hash(user.getPassword()));
+            userRepository.create(user);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            logger.log(Level.ERROR, "Failed to hash password", e);
-            throw new UserServiceException(e);
+            LOG.error("Failed to hash new user's password", e);
+            throw new UserServiceException(e.getMessage());
         } catch (DBException e) {
-            logger.log(Level.ERROR, "Failed to signup new User", e);
-            throw new UserServiceException(e);
+            throw new UserServiceException(e.getMessage());
         }
     }
 
     @Override
     public User login(User user) throws UserServiceException {
-        Optional<User> loggedUser = Optional.empty();
         try {
-            if (userRepository.isExist(user) &&
-                    userRepository.getPasswordById(user).equals(PasswordEncoder.hash(user.getPassword()))) {
-                loggedUser = userRepository.findById(user);
+            if (!userRepository.isExist(user)) {
+                throw new UserServiceException(String.format("User with provided login not exists, login=%s", user.getId()));
             }
+            if (!userRepository.getPasswordById(user).equals(PasswordEncoder.hash(user.getPassword()))) {
+                throw new UserServiceException("Invalid password. Didn't match with password from db");
+            }
+            return userRepository.findById(user).get();
         } catch (DBException e) {
-            logger.log(Level.ERROR, "Failed to login a user", e);
-            throw new UserServiceException(e);
+            throw new UserServiceException(e.getMessage());
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            logger.log(Level.ERROR, "Passwords didn't match", e);
-            throw new UserServiceException(e);
+            LOG.error("Failed to hash login user's password to compare", e);
+            throw new UserServiceException(e.getMessage());
         }
+    }
 
-        if (loggedUser.isEmpty()) {
-            throw new UserServiceException("No such user was found");
+    @Override
+    public List<User> findAllUsers() throws UserServiceException {
+        try {
+            return userRepository.readAllUsers();
+        } catch (DBException e) {
+            throw new UserServiceException(e.getMessage());
         }
-        return loggedUser.get();
     }
 }
