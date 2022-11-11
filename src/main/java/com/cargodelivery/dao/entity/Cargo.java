29698be8a5f0da43b38d81e045b8d71790ebf1fc
MinoUni <maks.reva.ua@gmail.com>
2022-11-11 13:49:47 +0200

    Add toString, equals & hashcode

--- src/main/java/com/cargodelivery/dao/entity/Cargo.java
+++ src/main/java/com/cargodelivery/dao/entity/Cargo.java
@@ -1,34 +1,13 @@
 package com.cargodelivery.dao.entity;
 
-/**
- * Model that represent MySQL cargo table
- * and easier to use
- * Contains 2 constructors:
- *  1. Wrap all parameters received from request
- *     with model-object;
- *  2. Build a model-object from data from database
- *
- */
+import java.util.Objects;
+
 public class Cargo {
 
-    private int id;
-
     private final double length;
-
     private final double width;
-
     private final double height;
-
     private final double weight;
-
-    // Wrap data about cargo from database cargo table into 1 model-object
-    public Cargo(int id, double length, double width, double height, double weight) {
-        this.id = id;
-        this.length = length;
-        this.width = width;
-        this.height = height;
-        this.weight = weight;
-    }
 
     // Wrap data about cargo from Http request into model-object for better use
     public Cargo(double length, double width, double height, double weight) {
@@ -36,10 +15,6 @@ public class Cargo {
         this.width = width;
         this.height = height;
         this.weight = weight;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public double getLength() {
@@ -58,4 +33,21 @@ public class Cargo {
         return weight;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cargo cargo = (Cargo) o;
+        return Double.compare(cargo.length, length) == 0 && Double.compare(cargo.width, width) == 0 && Double.compare(cargo.height, height) == 0 && Double.compare(cargo.weight, weight) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length, width, height, weight);
+    }
+
+    @Override
+    public String toString() {
+        return  String.format("Cargo{length=%.2f, width=%.2f, height=%.2f, weight=%.2f}", length, width, height, weight);
+    }
 }
