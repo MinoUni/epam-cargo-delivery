4e94ef9cb12cd0b9567d8a4d81370ec8243049a6
MinoUni <maks.reva.ua@gmail.com>
2022-11-22 11:32:30 +0200

    Refactor commands init

--- src/main/java/com/cargodelivery/controller/command/CommandList.java
+++ src/main/java/com/cargodelivery/controller/command/CommandList.java
@@ -4,16 +4,20 @@ import com.cargodelivery.controller.command.impl.get.*;
 import com.cargodelivery.controller.command.impl.post.CreateOrder;
 import com.cargodelivery.controller.command.impl.post.Login;
 import com.cargodelivery.controller.command.impl.post.Signup;
+import com.cargodelivery.dao.impl.OrderDaoImpl;
+import com.cargodelivery.dao.impl.UserDaoImpl;
+import com.cargodelivery.service.impl.OrderServiceImpl;
+import com.cargodelivery.service.impl.UserServiceImpl;
 
 public enum CommandList {
 
     ERROR_PAGE(new ErrorPage()),
-    SIGNUP(new Signup()),
-    LOGIN(new Login()),
+    SIGNUP(new Signup(new UserServiceImpl(new UserDaoImpl()))),
+    LOGIN(new Login(new UserServiceImpl(new UserDaoImpl()))),
     LOGOUT(new Logout()),
     LOCALE(new Locale()),
-    CREATE_ORDER(new CreateOrder()),
-    USER_ORDERS(new UserOrders()),
+    CREATE_ORDER(new CreateOrder(new OrderServiceImpl(new OrderDaoImpl(), new UserDaoImpl()))),
+    USER_ORDERS(new UserOrders(new OrderServiceImpl(new OrderDaoImpl(), new UserDaoImpl()))),
     ORDER_INFO(new OrderInfo()),
     ORDER_DELETE(new OrderDelete()),
     ORDER_PAY(new OrderPay()),
